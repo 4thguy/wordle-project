0e22616811924579b2befa1122d932188444eb13
4thguy <ivanxuereb@gmail.com>
2024-02-29 14:40:17 +0100

    watch server development files

--- wordle-server/src/server.ts
+++ wordle-server/src/server.ts
@@ -8,29 +8,29 @@ const app = express();
 app.use(express.json()); // Parse JSON bodies
 
 function onConfigRequested(): Config {
-    // if  the config is not spread, the result is empty
-    return {
-        ...Config,
-        WordId: DictionaryLogic.getTodaysWordId(),
-    };
+	// if  the config is not spread, the result is empty
+	return {
+		...Config,
+		WordId: DictionaryLogic.getTodaysWordId(),
+	};
 }
 
 function onGuessSubmitted(data: any): Guess {
-    return ServerLogic.validateGuess(data.guess, data.wordId);
+	return ServerLogic.validateGuess(data.guess, data.wordId);
 }
 
 app.get(Config.Endpoints.ROOT, (req: Request, res: Response) => {
-    res.json(onConfigRequested());
+	res.json(onConfigRequested());
 });
 app.get(Config.Endpoints.CONFIG, (req: Request, res: Response) => {
-    res.json(onConfigRequested());
+	res.json(onConfigRequested());
 });
 
 app.post(Config.Endpoints.GUESS, (req: Request, res: Response) => {
-    res.json(onGuessSubmitted(req.body));
+	res.json(onGuessSubmitted(req.body));
 });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+	console.log(`Server is running on port ${port}`);
 });
