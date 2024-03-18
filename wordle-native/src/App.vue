3313f3f5380c3f26ebe58c1cd4f68d56eeee7360
4thguy <ivanxuereb@gmail.com>
2024-03-18 15:25:53 +0100

    basic keyboard for wordle-native

--- wordle-native/src/App.vue
+++ wordle-native/src/App.vue
@@ -1,43 +1,49 @@
 <template>
-  <Page>
-    <ActionBar>
-      <Label text="Home"/>
-    </ActionBar>
+	<Page>
+		<ActionBar>
+			<Label text="Home" />
+		</ActionBar>
 
-    <GridLayout>
-      <Label class="info">
-        <FormattedString>
-          <Span class="fas" text.decode="&#xf135; "/>
-          <Span :text="message"/>
-        </FormattedString>
-      </Label>
-    </GridLayout>
-  </Page>
+		<GridLayout>
+			<Label class="info">
+				<FormattedString>
+					<Span class="fas" text.decode="&#xf135; " />
+					<Span :text="message" />
+				</FormattedString>
+			</Label>
+
+			<Keyboard />
+		</GridLayout>
+	</Page>
 </template>
 
 <script lang="ts">
-  import Vue from "nativescript-vue";
+import Vue from 'nativescript-vue';
+import Keyboard from './components/Keyboard.vue';
 
-  export default Vue.extend({
-    computed: {
-      message() {
-        return "Wordle Native Test";
-      }
-    }
-  });
+export default Vue.extend({
+	components: {
+		Keyboard,
+	},
+	computed: {
+		message() {
+			return 'Wordle Native Test';
+		},
+	},
+});
 </script>
 
 <style scoped lang="scss">
-  @import '@nativescript/theme/scss/variables/blue';
+@import '@nativescript/theme/scss/variables/blue';
 
-  // Custom styles
-  .fas {
-    @include colorize($color: accent);
-  }
+// Custom styles
+.fas {
+	@include colorize($color: accent);
+}
 
-  .info {
-    font-size: 20;
-    horizontal-align: center;
-    vertical-align: center;
-  }
+.info {
+	font-size: 20;
+	horizontal-align: center;
+	vertical-align: center;
+}
 </style>
---
+++ wordle-native/src/components/Keyboard.vue
@@ -0,0 +1,123 @@
+<template>
+	<FlexboxLayout
+		flexDirection="row"
+		justifyContent="center"
+		flexWrap="wrap"
+		alignItems="center"
+		class="buttons buttons--qwerty"
+		@click="onPressButton"
+	>
+		<Button
+			v-for="l in letters"
+			:class="['button', `button--${l}`, getLetterStatusClass(l)]"
+			:order="getButtonOrder(l)"
+			:text="l"
+		/>
+		<Button class="button button--enter" :order="getButtonOrder('ENTER')" text="ENTER" />
+		<Button class="button button--backspace" :order="getButtonOrder('DEL')" text="DEL" />
+		<Label
+			class="button button--line-break button--line-break-1"
+			:order="getButtonOrder('LINE-BREAK-1')"
+			width="100%"
+			text=""
+		/>
+		<Label
+			class="button button--line-break button--line-break-2"
+			:order="getButtonOrder('LINE-BREAK-1')"
+			width="100%"
+			text=""
+		/>
+	</FlexboxLayout>
+</template>
+
+<script lang="ts">
+const letters = Array.from({ length: 26 }, (_, i) => String.fromCharCode(65 + i).toLowerCase());
+const alphabetical = [
+	'a',
+	'b',
+	'c',
+	'd',
+	'e',
+	'f',
+	'g',
+	'h',
+	'i',
+	'j',
+	'line-break-1',
+	'k',
+	'l',
+	'm',
+	'n',
+	'o',
+	'p',
+	'q',
+	'r',
+	's',
+	'line-break-2',
+	'enter',
+	't',
+	'u',
+	'v',
+	'w',
+	'x',
+	'y',
+	'z',
+	'del',
+];
+const qwerty = [
+	'q',
+	'w',
+	'e',
+	'r',
+	't',
+	'y',
+	'u',
+	'i',
+	'o',
+	'p',
+	'line-break-1',
+	'a',
+	's',
+	'd',
+	'f',
+	'g',
+	'h',
+	'j',
+	'k',
+	'l',
+	'line-break-2',
+	'enter',
+	'z',
+	'x',
+	'c',
+	'v',
+	'b',
+	'n',
+	'm',
+	'del',
+];
+
+export default {
+	name: 'Keyboard',
+	data() {
+		return {
+			letters: letters,
+		};
+	},
+	methods: {
+		onPressButton(): void {},
+		getButtonOrder(s: string): number {
+			return qwerty.indexOf(s.toLocaleLowerCase()) ?? -1;
+		},
+		getLetterStatusClass(s: string): string {
+			return s;
+		},
+	},
+};
+</script>
+
+<style scoped>
+.button {
+	text-transform: uppercase;
+}
+</style>
